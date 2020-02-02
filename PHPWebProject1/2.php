2848d6205f49b7fd5bb7cadc48071512db673f0a
wojciechowski.bartlomiej <wojciechowski.bartlomiej@gmail.com>
2020-02-02 12:27:58 +0100

    test

---
+++ PHPWebProject1/2.php
@@ -0,0 +1,44 @@
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+<!------ Include the above in your HEAD tag ---------->
+
+
+        <style>
+
+
+            </style>
+
+    </head>
+
+    <body>
+         <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="images/office.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Card Title</span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+       <div class="card-action">...</div>
+
+    <div class="card-reveal">...</div>
+
+  </div>
+    </body>
+  </html>
+        
