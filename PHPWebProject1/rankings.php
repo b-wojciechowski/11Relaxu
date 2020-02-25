8b220e3c2ad04369c2af0bc7bed60db1dfc7e9d4
wojciechowski.bartlomiej <wojciechowski.bartlomiej@gmail.com>
2020-02-25 22:50:28 +0100

    formatted files.

--- PHPWebProject1/rankings.php
+++ PHPWebProject1/rankings.php
@@ -24,41 +24,23 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.2/js/materialize.min.js"></script>
- <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-     <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>-->
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<!--<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+<head>
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>-->
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!--<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>-->
 
 
 
-<script>
-    function showToast(message, duration) {
-    Materialize.toast(message, duration);
-    }
-         
-    function showToast1(message, duration) {
-    Materialize.toast('<i>'+ message + '</i>', duration);
-    }
-         
-    function showToast2(message, duration) {
-    Materialize.toast(message, duration, 'rounded');
-    }
-         
-    function showToast3(message, duration) {
-    Materialize.toast('Hello World!', duration, '', function toastCompleted() {
-        alert('Toast dismissed!');
-    });
-    }
-</script>
-	
+
 
 </head>
 
@@ -66,48 +48,56 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 <body>
 
 
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="js/materialize.min.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
 
- <?php include 'preloader.php'; ?>	
-    
-<div class="row">
-     
- <div class="col s12" style="padding:0px;">
-	   
-<!--START MENU--> 
-	 <?php include 'menu.php'; ?>
- <!--KONIEC MENU-->      
+	<?php include 'preloader.php'; ?>
 
-</div>
+	<div class="row">
 
-    <div class="col s12 m4 l3 hide-on-med-and-down"></div>
-    
-<div class="col s12 m4 l6" style="padding:0px;">
-	<div class="" style="background-color:white; margin-top:10px;">
-			
+		<div class="col s12" style="padding:0px;">
+
+			<!--START MENU-->
+			<?php include 'menu.php'; ?>
+			<!--KONIEC MENU-->
+
+		</div>
+
+		<div class="col s12 m4 l3 hide-on-med-and-down"></div>
+
+		<div class="col s12 m4 l6" style="padding:0px;">
+			<div class="" style="background-color:white; margin-top:10px;">
 
 
-<div style="padding:10px 10px 0px 15px;">
-	<p style="margin-bottom: -10px;
+
+				<div style="padding:10px 10px 0px 15px;">
+					<p style="margin-bottom: -10px;
     font-size: 12px;
     color: darkgrey;">Poniżej przedstawione są dotychczasowe liczby głosów</p>
-					<div><h4><b>Rankingi</b></h4></div>		
+					<div>
+						<h4><b>Rankingi</b></h4>
 					</div>
-	
-						
-<div style="padding:10px 10px 5px 10px;">
-					<div><i class="material-icons left">people</i><h5><b>Bramkarze</b></h5></div>		
-			
-			</div>		
-			
+				</div>
 
-  <ul class="collection collapsible">
-	
 
-			<?php
+				<div style="padding:10px 10px 5px 10px;">
+					<div><i class="material-icons left">people</i>
+						<h5><b>Bramkarze</b></h5>
+					</div>
+
+				</div>
+
+
+				<ul class="collection collapsible">
+
+
+					<?php
 			
-			$sql = "select p.Id as Id, p.Name as Name, p.LastName as LastName, count(1) as Votes, p.Position as Position  from votes as v join players as p on p.id = v.VoteOnPlayerId where Position = 'bramkarz' and v.IsDeleted = 0  group by p.Id order by count(1) desc";
+			$sql = "select p.Id as Id, p.Name as Name, p.LastName as LastName, count(1) as Votes, p.Position as Position from votes as v 
+					join players as p on p.id = v.VoteOnPlayerId 
+					where Position = 'bramkarz' 
+						and v.IsDeleted = 0  
+					group by p.Id 
+					order by count(1) desc";
 			$result = $polaczenie->query($sql);
 
 			if ($result->num_rows > 0)
@@ -121,56 +111,62 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 					$lastname = $row["LastName"];
 					$position = $row["Position"];
 					$votes = $row["Votes"];
-					$deafultPhoto = "onerror= this.onerror=null;this.src='images/default.jpg';";
-			
 					
+					$playerImage = (playerImgName($name, $lastname));	
+					if (!in_array($playerImage, $player_images)) 
+					{
+						$playerImage = 'default.jpg';	
+					}
 
 					
 					echo
 
 '<li>
-	  <div class="collapsible-header"style="padding:0px">
-	  <div class="collection-item avatar" style="width:100%";>
-		
-      <img class="circle"'.$deafultPhoto.' src="images/'.playerImgName($name, $lastname).'" style="paddnig-right:10px;"/>
-      <div style="padding-left:10px;"> <span class="title">'.$name." ".$lastname.'</span> </div>
+	<div class="collapsible-header" style="padding:0px">
+		<div class="collection-item avatar" style="width:100%" ;>
 
-      <p style="padding-left:10px; color:grey; margin-top:10px;">   <i class="material-icons left"  style="margin-top:-2px; margin-right:0px!important;">star_border</i> Liczba głosów: '.$votes.'
+			<img class="circle" src="images/'.$playerImage.'" style="paddnig-right:10px;" />
+			<div style="padding-left:10px;"> <span class="title">'.$name." ".$lastname.'</span> </div>
 
-      </p>
-      <a href="#!" class="secondary-content"><i class="material-icons">expand_more</i></a>
+			<p style="padding-left:10px; color:grey; margin-top:10px;"> <i class="material-icons left"
+					style="margin-top:-2px; margin-right:0px!important;">star_border</i> Liczba głosów: '.$votes.'
+
+			</p>
+			<a href="#!" class="secondary-content"><i class="material-icons">expand_more</i></a>
 
 		</div>
-		</div>
-      <div class="collapsible-body">
+	</div>
+	<div class="collapsible-body">
 
-<a class="waves-effect waves-light">Dodaj informacje o zawodniku</a>
+		<a class="waves-effect waves-light">Dodaj informacje o zawodniku</a>
 
-<br>
-</div>
+		<br>
+	</div>
 
 </li>';
-				}
-			}
-			else
-			{
-				echo '<p style="padding-left:15px;margin-top:0px;">Nie oddano głosu na bramkarzy.</p>';
-			};
+}
+}
+else
+{
+echo '<p style="padding-left:15px;margin-top:0px;">Nie oddano głosu na bramkarzy.</p>';
+};
 
-            ?>
+?>
 
 </ul>
-		
+
 <div style="padding:10px 10px 5px 10px;">
-					<div> <i class="material-icons left">people</i><h5><b>Obrońcy</b></h5></div>		
-					<!--Zaznacz "Jedenastka 50-lecia" przy 1 zawodniku.-->
-			</div>		
-			
+	<div> <i class="material-icons left">people</i>
+		<h5><b>Obrońcy</b></h5>
+	</div>
+	<!--Zaznacz "Jedenastka 50-lecia" przy 1 zawodniku.-->
+</div>
 
-  <ul class="collection collapsible">
-	
 
-			<?php
+<ul class="collection collapsible">
+
+
+	<?php
 			require_once "sql/connection.php";
 		
 			$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
@@ -192,32 +188,37 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 					$lastname = $row["LastName"];
 					$position = $row["Position"];
 					$votes = $row["Votes"];
-					$deafultPhoto = "onerror= this.onerror=null;this.src='images/default.jpg';";
 					
+					$playerImage = (playerImgName($name, $lastname));	
+					if (!in_array($playerImage, $player_images)) 
+					{
+						$playerImage = 'default.jpg';	
+					}
 	
 					echo
 
 '<li>
-	  <div class="collapsible-header"style="padding:0px">
-	  <div class="collection-item avatar" style="width:100%";>
-		
-      <img class="circle"'.$deafultPhoto.' src="images/'.playerImgName($name, $lastname).'" style="paddnig-right:10px;"/>
-      <div style="padding-left:10px;"> <span class="title">'.$name." ".$lastname.'</span> </div>
+	<div class="collapsible-header" style="padding:0px">
+		<div class="collection-item avatar" style="width:100%" ;>
 
-    
-      <p style="padding-left:10px; color:grey; margin-top:10px;">   <i class="material-icons left"  style="margin-top:-2px; margin-right:0px!important;">star_border</i> Liczba głosów: '.$votes.'
+			<img class="circle" src="images/'.$playerImage.'" style="paddnig-right:10px;" />
+			<div style="padding-left:10px;"> <span class="title">'.$name." ".$lastname.'</span> </div>
 
-      </p>
-      <a href="#!" class="secondary-content"><i class="material-icons">expand_more</i></a>
+
+			<p style="padding-left:10px; color:grey; margin-top:10px;"> <i class="material-icons left"
+					style="margin-top:-2px; margin-right:0px!important;">star_border</i> Liczba głosów: '.$votes.'
+
+			</p>
+			<a href="#!" class="secondary-content"><i class="material-icons">expand_more</i></a>
 
 		</div>
-		</div>
-     <div class="collapsible-body">
+	</div>
+	<div class="collapsible-body">
 
-<a class="waves-effect waves-light">Dodaj informacje o zawodniku</a>
+		<a class="waves-effect waves-light">Dodaj informacje o zawodniku</a>
 
-<br>
-</div>
+		<br>
+	</div>
 
 </li>';
 				}
@@ -229,22 +230,24 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 
             ?>
 
-</ul>
+				</ul>
 
 
 
 
 
-	
-<div style="padding:10px 10px 5px 10px;">
-					<div> <i class="material-icons left">people</i><h5><b>Pomocnicy</b></h5></div>		
+
+				<div style="padding:10px 10px 5px 10px;">
+					<div> <i class="material-icons left">people</i>
+						<h5><b>Pomocnicy</b></h5>
+					</div>
 					<!--Zaznacz "Jedenastka 50-lecia" przy 1 zawodniku.-->
-			</div>	
+				</div>
 
-  <ul class="collection collapsible">
-	
+				<ul class="collection collapsible">
 
-			<?php
+
+					<?php
 			require_once "sql/connection.php";
 			
 			$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
@@ -267,7 +270,11 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 					$position = $row["Position"];
 					$votes = $row["Votes"];
 					$deafultPhoto = "onerror= this.onerror=null;this.src='images/default.jpg';";
-					
+					$playerImage = (playerImgName($name, $lastname));	
+					if (!in_array($playerImage, $player_images)) 
+					{
+						$playerImage = 'default.jpg';	
+					}
 					
 					echo
 
@@ -275,7 +282,7 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 	  <div class="collapsible-header"style="padding:0px">
 	  <div class="collection-item avatar" style="width:100%";>
 		
-      <img class="circle"'.$deafultPhoto.' src="images/'.playerImgName($name, $lastname).'" style="paddnig-right:10px;"/>
+      <img class="circle" src="images/'.$playerImage.'" style="paddnig-right:10px;"/>
       <div style="padding-left:10px;"> <span class="title">'.$name." ".$lastname.'</span> </div>
 
    
@@ -303,23 +310,25 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 
             ?>
 
-</ul>
+				</ul>
 
 
 
 
 
-			
-		
-<div style="padding:10px 10px 5px 10px;">
-					<div> <i class="material-icons left">people</i><h5><b>Napastnicy</b></h5></div>		
+
+
+				<div style="padding:10px 10px 5px 10px;">
+					<div> <i class="material-icons left">people</i>
+						<h5><b>Napastnicy</b></h5>
+					</div>
 					<!--Zaznacz "Jedenastka 50-lecia" przy 1 zawodniku.-->
-			</div>	
+				</div>
 
-  <ul class="collection collapsible">
-	
+				<ul class="collection collapsible">
 
-			<?php
+
+					<?php
 			require_once "sql/connection.php";
 			
 			$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
@@ -341,7 +350,12 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 					$lastname = $row["LastName"];
 					$position = $row["Position"];
 					$votes = $row["Votes"];
-					$deafultPhoto = "onerror= this.onerror=null;this.src='images/default.jpg';";
+					
+					$playerImage = (playerImgName($name, $lastname));	
+					if (!in_array($playerImage, $player_images)) 
+					{
+						$playerImage = 'default.jpg';	
+					}
 					
 					
 					echo
@@ -350,7 +364,7 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 	  <div class="collapsible-header"style="padding:0px">
 	  <div class="collection-item avatar" style="width:100%";>
 		
-      <img class="circle"'.$deafultPhoto.' src="images/'.playerImgName($name, $lastname).'" style="paddnig-right:10px;"/>
+      <img class="circle" src="images/'.$playerImage.'" style="paddnig-right:10px;"/>
       <div style="padding-left:10px;"> <span class="title">'.$name." ".$lastname.'</span> </div>
 
    
@@ -378,26 +392,7 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 
             ?>
 
-</ul>
-
-
-
-		
-
-
-
-
-
-
-
-
-				
-
-
-
-
-
-	
+				</ul>
 
 
 
@@ -411,46 +406,63 @@ $result_insert_activity = $polaczenie->query($sql_activity);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			</div>
+
+		</div>
+
+		<div class="col s12 m4 l3 hide-on-med-and-down"></div>
 	</div>
 
-</div>
 
-   <div class="col s12 m4 l3 hide-on-med-and-down"></div>
-  </div>
-
-
-  <div class="row">
-    <div class="col s12 m4 l2 hide-on-med-and-down"></div>
-    <div class="col s12 m4 l8"> <?php include 'footer.php'; ?></div>
-    <div class="col s12 m4 l2 hide-on-med-and-down"></div>
-  </div>
-          
-          
- 
-<script>
-	(function($){
-	$(function(){
-
-	$('.button-collapse').sideNav({
-		edge: 'left', // Choose the horizontal origin
-		closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-		draggable: true // Choose whether you can drag to open on touch screens
-	}
-	);
-
-	}); // end of document ready
-	})(jQuery); // end of jQuery name space
-</script>
+	<div class="row">
+		<div class="col s12 m4 l2 hide-on-med-and-down"></div>
+		<div class="col s12 m4 l8"> <?php include 'footer.php'; ?></div>
+		<div class="col s12 m4 l2 hide-on-med-and-down"></div>
+	</div>
 
 
 
-<script>
+	<script>
+		(function ($) {
+			$(function () {
+
+				$('.button-collapse').sideNav({
+					edge: 'left', // Choose the horizontal origin
+					closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+					draggable: true // Choose whether you can drag to open on touch screens
+				});
+
+			}); // end of document ready
+		})(jQuery); // end of jQuery name space
+	</script>
 
 
-  $(document).ready(function(){
-    $('.collapsible').collapsible();
-  }); 
+
+	<script>
+		$(document).ready(function () {
+			$('.collapsible').collapsible();
+		});
 	</script>
 
 </body>
+
 </html>
--- PHPWebProject1/tools/tools.php
+++ PHPWebProject1/tools/tools.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * tools short summary.
  *
@@ -8,41 +7,30 @@
  * @version 1.0
  * @author Bartek
  */
+
 function playerImgName($_name, $_lastName)
 {
-
-    $FileName = replacePolishCharacters($_name.'-'.$_lastName).'.jpg';
-
-    return $FileName;
+	$FileName = replacePolishCharacters($_name . '-' . $_lastName) . '.jpg';
+	return $FileName;
 }
 
-function replacePolishCharacters($string){
-	$a = array( 'Ę', 'Ó', 'Ą', 'Ś', 'Ł', 'Ż', 'Ź', 'Ć', 'Ń', 'ę', 'ó', 'ą',
-				'ś', 'ł', 'ż', 'ź', 'ć', 'ń' );
-	$b = array( 'E', 'O', 'A', 'S', 'L', 'Z', 'Z', 'C', 'N', 'e', 'o', 'a',
-				's', 'l', 'z', 'z', 'c', 'n' );
+function replacePolishCharacters($string)
+{
+	$a = array(
+		'Ę', 'Ó', 'Ą', 'Ś', 'Ł', 'Ż', 'Ź', 'Ć', 'Ń', 'ę', 'ó', 'ą',
+		'ś', 'ł', 'ż', 'ź', 'ć', 'ń'
+	);
+	$b = array(
+		'E', 'O', 'A', 'S', 'L', 'Z', 'Z', 'C', 'N', 'e', 'o', 'a',
+		's', 'l', 'z', 'z', 'c', 'n'
+	);
 
-	$string = str_replace( $a, $b, $string );
-	$string = preg_replace( '#[^a-z0-9]#is', ' ', $string );
-	$string = trim( $string );
-	$string = preg_replace( '#\s{2,}#', ' ', $string );
-	$string = str_replace( ' ', '-', $string );
+	$string = str_replace($a, $b, $string);
+	$string = preg_replace('#[^a-z0-9]#is', ' ', $string);
+	$string = trim($string);
+	$string = preg_replace('#\s{2,}#', ' ', $string);
+	$string = str_replace(' ', '-', $string);
 	$string = strtolower($string);
+	
 	return $string;
 }
-$playerImage = "michal-kosmider.jpg";
-function checkPlayerImage($playerImage){
-	$img_dir = '/images';
-	$player_images = scandir($img_dir);
-	print_r($player_images);
-	if (in_array($playerImage, $player_images)) {
-		$playerImage = $playerImage;
-		echo $playerImage;
-	}
-	else {
-		$playerImage = 'default.jpg';
-		echo $playerImage;
-	}
-
-}
-?>
